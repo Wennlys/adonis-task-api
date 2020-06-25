2806c6f400588618e763c478f1f73bc3655cac13
Wennlys Oliveira <wennlys@gmail.com>
2020-06-25 10:16:29 -0300

    Task email hook.

---
+++ app/Models/Hooks/TaskHook.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Mail = use('Mail')
+const Helpers = use('Helpers')
+const TaskHook = exports = module.exports = {}
+
+TaskHook.sendNewTaskMail = async taskInstance => {
+  if (taskInstance.user_id && taskInstance.dirty.user_id) return
+
+  const { email, username } = await taskInstance.user().fetch()
+
+  const file = await taskInstance.file().fetch()
+
+  const { title } = taskInstance
+
+  await Mail.send(
+    ['emails.new_task'],
+    { username, title, hasAttachment: !!file },
+    message => {
+      message
+        .to(email)
+        .from('wennlys@gmail.com', 'Wennlys Oliveira')
+        .subject('New task!')
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        })
+      }
+    }
+  )
+}
