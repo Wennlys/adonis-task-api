72509b25cd57aeda49989c7060f62b7311a5d613
Wennlys Oliveira <wennlys@gmail.com>
2020-06-25 04:14:01 -0300

    File uploading OK.

---
+++ app/Controllers/Http/FileController.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const File = use('App/Models/File')
+const Helpers = use('Helpers')
+
+/**
+ * Resourceful controller for interacting with files
+ */
+class FileController {
+  /**
+   * Create/save a new file.
+   * POST files
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+    try {
+      if (!request.file('file')) return
+
+      const upload = request.file('file', { size: '2mb' })
+
+      const fileName = `${Date.now()}.${upload.subtype}`
+
+      await upload.move(Helpers.tmpPath('uploads'), {
+        name: fileName
+      })
+
+      if (!upload.move()) {
+        throw upload.error()
+      }
+
+      const file = await File.create({
+        file: fileName,
+        name: upload.clientName,
+        type: upload.type,
+        subtype: upload.subtype
+      })
+
+      return file
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: `ERROR: ${err.status}` } })
+    }
+  }
+}
+
+module.exports = FileController
--- start/routes.js
+++ start/routes.js
@@ -5,5 +5,6 @@ const Route = use('Route')
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'PasswordRecoveryController.store')
+Route.post('files', 'FileController.store')
 
 Route.put('passwords', 'PasswordRecoveryController.update')
